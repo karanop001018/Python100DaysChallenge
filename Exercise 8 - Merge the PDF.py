edbe3a247d79fa3ab27d0f0061173302b375aff9
Karan Patel <96780293+karanop001018@users.noreply.github.com>
2023-09-26 01:25:01 +0530

    Add files via upload

---
+++ Exercise 8 - Merge the PDF.py
@@ -0,0 +1,13 @@
+# Write a program to manipulate pdf files using pyPDF. Your programs should be able to merge multiple pdf files into a single pdf. You are welcome to add more functionalities
+# pypdf is a free and open-source pure-python PDF library capable of splitting, merging, cropping, and transforming the pages of PDF files. It can also add custom data, viewing options, and passwords to PDF files. pypdf can retrieve text and metadata from PDFs as well.
+
+from PyPDF2 import PdfWriter
+import os
+merger = PdfWriter()
+files = [file from file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+    
+merger.write("Merged_Pdf.pdf")
+merger.close()
